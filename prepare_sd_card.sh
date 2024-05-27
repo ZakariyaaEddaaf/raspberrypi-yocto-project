a7f59a1b763606bcfb2e4cdbd500482bda3faeb5
zakariyaa.eddaaf <zakariyaa.eddaaf@evologics.de>
2024-05-27 22:32:29 +0200

    umount partition function

--- prepare_sd_card.sh
+++ prepare_sd_card.sh
@@ -4,18 +4,35 @@ NUM_OF_ARGS=$#
 usage()
 {
     if [ "${NUM_OF_ARGS}" -ne 1 ];then
-            echo "Usage: $(basename $0) <action>"
+            echo ">> Usage: $(basename $0) <action>"
             exit 1
     fi
 }
 require_sudo_privilege() 
 {
     if [[ $EUID -ne 0 ]]; then
-        echo "This script requires sudo privileges."
+        echo ">> This script requires sudo privileges."
         exit 1
     fi
 }
 
+umount_partitions() 
+{
+    echo ">> Unmounting partitions..."
+    if ls /dev/sda* >/dev/null 2>&1; then
+        for partition in /dev/sda*; do
+            if [ -b "$partition" ]; then
+                umount "$partition" 2>/dev/null && echo ">> Unmounted $partition"
+            fi
+        done
+        echo ">> Partitions unmounted"
+    else
+        echo ">> No partitions found for unmounting."
+    fi
+}
+
+
 echo "[=============== Prepare Sd-Card Tool ===============]"
 usage
 require_sudo_privilege
+umount_partitions
